950fb7251778a798091271d60232cf8d9d10c8f1
devedeepak <dpksingh489@gmail.com>
2025-12-01 19:04:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const addCount = () =>{
+    setCount((count) => count + 1)
+  }
+
+  const reduceCount = () =>{
+    setCount((count) => count>0 ? count - 1 : count)
+  }
+
+  return (
+    <>
+      <h1>CountDown</h1>
+      <div className="card">
+        <div style={{marginBottom: '10px',fontSize:'2rem'}}>count is {count}</div>
+        <button onClick={() => addCount()} style={{marginRight: '10px',backgroundColor: "green"}}>
+          +
+        </button>
+        <button onClick={() => reduceCount()} style={{backgroundColor: "red"}}>
+          -
+        </button> 
+      </div>
+    </>
+  )
+}
+
+export default App
